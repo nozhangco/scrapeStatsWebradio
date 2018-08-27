59316d8a668c845df773b4b9515e30e905137138
nozhangco <91894496+nozhangco@users.noreply.github.com>
2018-08-27 08:59:19 +0000

    merges from trunk

---
+++ android/src/main/java/com/stario10module/BezelSettingBuilderWrapper.kt
@@ -0,0 +1,8 @@
+package com.stario10module
+
+import com.facebook.react.bridge.*
+import com.starmicronics.stario10.starxpandcommand.BezelSettingBuilder
+
+class BezelSettingBuilderWrapper internal constructor(context: ReactApplicationContext) : ReactContextBaseJavaModule(context) {
+    override fun getName(): String {
+ 

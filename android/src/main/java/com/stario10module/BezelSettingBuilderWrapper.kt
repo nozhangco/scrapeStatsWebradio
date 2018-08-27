9e37571bb0c4c4a03b799b2e34a0fff8d983a5ff
nozhangco <91894496+nozhangco@users.noreply.github.com>
2018-08-27 11:55:18 +0000

    diag: Reduce error message frequency

--- android/src/main/java/com/stario10module/BezelSettingBuilderWrapper.kt
+++ android/src/main/java/com/stario10module/BezelSettingBuilderWrapper.kt
@@ -5,4 +5,30 @@ import com.starmicronics.stario10.starxpandcommand.BezelSettingBuilder
 
 class BezelSettingBuilderWrapper internal constructor(context: ReactApplicationContext) : ReactContextBaseJavaModule(context) {
     override fun getName(): String {
- 
+        return "BezelSettingBuilderWrapper"
+    }
+
+    @ReactMethod
+    fun init(promise: Promise) {
+        val builder = BezelSettingBuilder()
+        val identifier = InstanceManager.set(builder)
+
+        promise.resolve(identifier)
+    }
+
+    @ReactMethod
+    fun dispose(identifier: String, promise: Promise) {
+        InstanceManager.remove(identifier)
+        promise.resolve(0)
+    }
+
+    @ReactMethod
+    fun settingAutomaticPageLength(identifier: String, enabled: Boolean, promise: Promise) {
+        val builder = InstanceManager.get(identifier)
+
+        if (builder is BezelSettingBuilder) {
+            builder.settingAutomaticPageLength(enabled)
+            promise.resolve(0)
+        }
+        else {
+            promise.reject(ReactNoCrashSoftExcep

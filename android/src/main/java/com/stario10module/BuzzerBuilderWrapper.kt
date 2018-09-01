73ea60aa0300b1410b1fe5c27219c388c7a1bcdf
nozhangco <91894496+nozhangco@users.noreply.github.com>
2018-09-01 13:01:52 +0000

    intelli_plug: add screen off max controls

--- android/src/main/java/com/stario10module/BuzzerBuilderWrapper.kt
+++ android/src/main/java/com/stario10module/BuzzerBuilderWrapper.kt
@@ -3,4 +3,28 @@ package com.stario10module
 import com.facebook.react.bridge.*
 import com.starmicronics.stario10.starxpandcommand.BuzzerBuilder
 
-class BuzzerBuilderWrapper inter
+class BuzzerBuilderWrapper internal constructor(context: ReactApplicationContext) : ReactContextBaseJavaModule(context) {
+    override fun getName(): String {
+        return "BuzzerBuilderWrapper"
+    }
+
+    @ReactMethod
+    fun init(promise: Promise) {
+        val builder = BuzzerBuilder()
+        val identifier = InstanceManager.set(builder)
+
+        promise.resolve(identifier)
+    }
+
+    @ReactMethod
+    fun dispose(identifier: String, promise: Promise) {
+        InstanceManager.remove(identifier)
+        promise.resolve(0)
+    }
+
+    @ReactMethod
+    fun actionDrive(identifier: String, channel: String, repeat: Int, onTime: Int, offTime: Int, promise: Promise) {
+        val builder = InstanceManager.get(identifier)
+
+        if (builder is BuzzerBuilder) {
+    

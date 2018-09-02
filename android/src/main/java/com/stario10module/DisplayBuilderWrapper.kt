de47314f8b4d87f7aa11f08749a42cfe6f2dd9bc
nozhangco <91894496+nozhangco@users.noreply.github.com>
2018-09-02 13:31:36 +0000

    update colors

--- android/src/main/java/com/stario10module/DisplayBuilderWrapper.kt
+++ android/src/main/java/com/stario10module/DisplayBuilderWrapper.kt
@@ -11,4 +11,26 @@ class DisplayBuilderWrapper internal constructor(context: ReactApplicationContex
     }
 
     @ReactMethod
-    fun init(promise: Pro
+    fun init(promise: Promise) {
+        val builder = DisplayBuilder()
+        val identifier = InstanceManager.set(builder)
+
+        promise.resolve(identifier)
+    }
+
+    @ReactMethod
+    fun dispose(identifier: String, promise: Promise) {
+        InstanceManager.remove(identifier)
+        promise.resolve(0)
+    }
+
+    @ReactMethod
+    fun styleInternationalCharacter(identifier: String, type: String, promise: Promise) {
+        val builder = InstanceManager.get(identifier)
+
+        if (builder is DisplayBuilder) {
+            builder.styleInternationalCharacter(StarIO10ValueConverter.toDisplayInternationalCharacterType(type))
+            promise.resolve(0)
+        }
+        else {
+            promise.reject(ReactNoCrashSoftEx

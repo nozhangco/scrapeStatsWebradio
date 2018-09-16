535476eddfe0cf95c33452d92ec977241aca8595
nozhangco <91894496+nozhangco@users.noreply.github.com>
2018-09-16 23:01:05 +0000

    chore: Upgrade dependencies

--- android/src/main/java/com/stario10module/MelodySpeakerBuilderWrapper.kt
+++ android/src/main/java/com/stario10module/MelodySpeakerBuilderWrapper.kt
@@ -1,1 +1,27 @@
-package c
+package com.stario10module
+
+import com.facebook.react.bridge.*
+import com.starmicronics.stario10.starxpandcommand.MelodySpeakerBuilder
+import com.starmicronics.stario10.StarIO10ArgumentException
+
+class MelodySpeakerBuilderWrapper internal constructor(context: ReactApplicationContext) : ReactContextBaseJavaModule(context) {
+    override fun getName(): String {
+        return "MelodySpeakerBuilderWrapper"
+    }
+
+    @ReactMethod
+    fun init(promise: Promise) {
+        val builder = MelodySpeakerBuilder()
+        val identifier = InstanceManager.set(builder)
+
+        promise.resolve(identifier)
+    }
+
+    @ReactMethod
+    fun dispose(identifier: String, promise: Promise) {
+        InstanceManager.remove(identifier)
+        promise.resolve(0)
+    }
+
+    @ReactMethod
+    fun act

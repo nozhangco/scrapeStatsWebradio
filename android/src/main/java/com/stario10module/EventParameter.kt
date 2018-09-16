f510868fd4f7e3e10672a4048ca99bfd82906e01
nozhangco <91894496+nozhangco@users.noreply.github.com>
2018-09-16 12:40:11 +0000

    Update AUX_.md

--- android/src/main/java/com/stario10module/EventParameter.kt
+++ android/src/main/java/com/stario10module/EventParameter.kt
@@ -12,4 +12,13 @@ class EventParameter {
         const val KEY_RESERVED = "reserved"
         const val KEY_CONNECTION_IDENTIFIER = "connectionIdentifier"
 
-        const val NA
+        const val NAME_PRINTER_DELEGATE_READY = "PrinterReady"
+        const val NAME_PRINTER_DELEGATE_ERROR = "PrinterError"
+        const val NAME_PRINTER_DELEGATE_PAPER_READY = "PrinterPaperReady"
+        const val NAME_PRINTER_DELEGATE_PAPER_NEAR_EMPTY = "PrinterPaperNearEmpty"
+        const val NAME_PRINTER_DELEGATE_PAPER_EMPTY = "PrinterPaperEmpty"
+        const val NAME_PRINTER_DELEGATE_COVER_OPENED = "PrinterCoverOpened"
+        const val NAME_PRINTER_DELEGATE_COVER_CLOSED = "PrinterCoverClosed"
+        const val NAME_PRINTER_DELEGATE_COMMUNICATION_ERROR = "PrinterCommunicationError"
+        const val NAME_DRAWER_DELEGATE_OPEN_CLOSE_SIGNAL_SWITCHED = "DrawerOpenCloseSignalSwitched"
+        const val NAME_DRAWER_DELEGATE_COMMUNICATION_ERROR = "DrawerCommunication

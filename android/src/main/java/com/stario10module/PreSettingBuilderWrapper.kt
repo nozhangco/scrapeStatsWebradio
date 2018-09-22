2b5127ebce114d35a889ebd3b83e4ac9d5466ca3
nozhangco <91894496+nozhangco@users.noreply.github.com>
2018-09-22 12:29:14 +0000

    Added copyright info

--- android/src/main/java/com/stario10module/PreSettingBuilderWrapper.kt
+++ android/src/main/java/com/stario10module/PreSettingBuilderWrapper.kt
@@ -1,4 +1,28 @@
 package com.stario10module
 
 import com.facebook.react.bridge.*
-import com.starmicronics.stario10.starxpandcommand.BezelSettingBui
+import com.starmicronics.stario10.starxpandcommand.BezelSettingBuilder
+import com.starmicronics.stario10.starxpandcommand.PreSettingBuilder
+import com.starmicronics.stario10.starxpandcommand.PresenterSettingBuilder
+
+class PreSettingBuilderWrapper internal constructor(context: ReactApplicationContext) : ReactContextBaseJavaModule(context) {
+    override fun getName(): String {
+        return "PreSettingBuilderWrapper"
+    }
+
+    @ReactMethod
+    fun init(promise: Promise) {
+        val builder = PreSettingBuilder()
+        val identifier = InstanceManager.set(builder)
+
+        promise.resolve(identifier)
+    }
+
+    @ReactMethod
+    fun dispose(identifier: String, promise: Promise) {
+        InstanceManager.remove(identifier)
+        promise.resolve(0)
+    }
+
+    @ReactMethod
+    fun addPresenterSetting(identifier

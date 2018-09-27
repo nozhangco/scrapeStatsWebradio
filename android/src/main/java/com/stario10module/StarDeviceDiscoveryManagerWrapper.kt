bd118a774e118ade776e28056aaa753211356298
nozhangco <91894496+nozhangco@users.noreply.github.com>
2018-09-27 16:26:46 +0000

    l10n: Updated German (de) translation to 100%

--- android/src/main/java/com/stario10module/StarDeviceDiscoveryManagerWrapper.kt
+++ android/src/main/java/com/stario10module/StarDeviceDiscoveryManagerWrapper.kt
@@ -16,4 +16,30 @@ class StarDeviceDiscoveryManagerWrapper internal constructor(context: ReactAppli
         val typeList = mutableListOf<InterfaceType>()
 
         for(type in StarIO10ValueConverter.toList<String>(interfaceTypes)) {
-            typeList.add(StarIO10ValueCo
+            typeList.add(StarIO10ValueConverter.toInterfaceType(type))
+        }
+
+        try {
+            val manager = StarDeviceDiscoveryManagerFactory.create(typeList, reactApplicationContext)
+            val identifier = InstanceManager.set(manager)
+
+            promise.resolve(identifier)
+        }
+        catch (e: StarIO10Exception) {
+            val exceptionIdentifier = InstanceManager.set(e)
+            promise.reject(exceptionIdentifier, e)
+        }
+    }
+
+    @ReactMethod
+    fun dispose(identifier: String, promise: Promise) {
+        InstanceManager.remove(identifier)
+        promise.resolve(0)
+    }
+
+    @ReactMethod
+    fun addListener(eventName: String) {
+        // Set up any upstream listeners or background tasks as necessary
+    }
+
+    @ReactMe

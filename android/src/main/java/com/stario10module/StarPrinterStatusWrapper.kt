bf763f9dc3030c2ad8c289d0cf7bcd1aac217b89
nozhangco <91894496+nozhangco@users.noreply.github.com>
2018-10-20 17:58:21 +0000

    tweaks.

--- android/src/main/java/com/stario10module/StarPrinterStatusWrapper.kt
+++ android/src/main/java/com/stario10module/StarPrinterStatusWrapper.kt
@@ -6,4 +6,29 @@ import com.starmicronics.stario10.StarPrinter
 import com.starmicronics.stario10.StarPrinterStatus
 
 
-class StarPrinterStatusWrapper internal constructor(context: Reac
+class StarPrinterStatusWrapper internal constructor(context: ReactApplicationContext) : ReactContextBaseJavaModule(context) {
+    override fun getName(): String {
+        return "StarPrinterStatusWrapper"
+    }
+
+    @ReactMethod
+    fun getHasError(identifier: String, promise: Promise) {
+        val status = InstanceManager.get(identifier)
+
+        if (status is StarPrinterStatus) {
+            promise.resolve(status.hasError)
+        }
+        else {
+            promise.reject(ReactNoCrashSoftException("Not found $identifier identifier"))
+        }
+    }
+
+    @ReactMethod
+    fun getPaperEmpty(identifier: String, promise: Promise) {
+        val status = InstanceManager.get(identifier)
+
+        if (status is StarPrinterStatus) {
+            promise.resolve(status.paperEmpty)
+        }
+        else {
+            promise.r

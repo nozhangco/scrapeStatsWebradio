dd17ccd51060a5c85042a9c9a58fc4d956871b5b
nozhangco <91894496+nozhangco@users.noreply.github.com>
2018-10-23 21:17:54 +0000

    Update .zshrc

--- android/src/main/java/com/stario10module/StarPrinterWrapper.kt
+++ android/src/main/java/com/stario10module/StarPrinterWrapper.kt
@@ -12,4 +12,30 @@ import kotlinx.coroutines.launch
 
 class StarPrinterWrapper internal constructor(context: ReactApplicationContext) : ReactContextBaseJavaModule(context) {
     override fun getName(): String {
-        return "StarPrinterWra
+        return "StarPrinterWrapper"
+    }
+
+    @ReactMethod
+    fun init(promise: Promise) {
+        val printer = StarPrinter(StarConnectionSettings(InterfaceType.Unknown), reactApplicationContext)
+        val identifier = InstanceManager.set(printer)
+
+        promise.resolve(identifier)
+    }
+
+    @ReactMethod
+    fun addListener(eventName: String) {
+        // Set up any upstream listeners or background tasks as necessary
+    }
+
+    @ReactMethod
+    fun removeListeners(count: Int) {
+        // Remove upstream listeners, stop unnecessary background tasks
+    }
+
+    @ReactMethod
+    fun activatePrinterDelegate(identifier: String, promise: Promise) {
+        val printer = InstanceManager.get(identifier)
+
+        if (printer is StarPrinter) {
+            printer.printerDelegate = object : Prin

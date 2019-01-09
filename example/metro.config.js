5b4e6887e7c41ea08e4e73771813c6bf74e585af
nozhangco <91894496+nozhangco@users.noreply.github.com>
2019-01-09 17:43:24 +0000

    fixed routing

--- example/metro.config.js
+++ example/metro.config.js
@@ -12,4 +12,18 @@ module.exports = {
     blacklistRE: blacklist([
       // This stops "react-native run-windows" from causing the metro server to crash if its already running
       new RegExp(
-        `${path.resolve(__di
+        `${path.resolve(__dirname, 'windows').replace(/[/\\]/g, '/')}.*`,
+      ),
+      // This prevents "react-native run-windows" from hitting: EBUSY: resource busy or locked, open msbuild.ProjectImports.zip
+      /.*\.ProjectImports\.zip/,
+    ]),
+  },
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: false,
+      },
+    }),
+  },
+};

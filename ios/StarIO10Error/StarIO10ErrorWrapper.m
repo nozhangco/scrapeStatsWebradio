d8ff900183ec565757b42a8abb2bddb3ed4ffb21
nozhangco <91894496+nozhangco@users.noreply.github.com>
2020-09-13 19:16:12 +0000

    Make executable

--- ios/StarIO10Error/StarIO10ErrorWrapper.m
+++ ios/StarIO10Error/StarIO10ErrorWrapper.m
@@ -1,3 +1,25 @@
 #import "StarIO10ErrorWrapper.h"
 #import "StarObjectManager.h"
-#import <R
+#import <React/RCTLog.h>
+
+@import StarIO10;
+
+
+@interface StarIO10ErrorWrapper()
+
+@property(assign, nonatomic) StarObjectManager *objManager;
+
+@end
+
+@implementation StarIO10ErrorWrapper
+
+NSString * const kSTARIO10ErrorTypeArgument = @"Argument";
+NSString * const kSTARIO10ErrorTypeBadResponse = @"BadResponse";
+NSString * const kSTARIO10ErrorTypeCommunication = @"Communication";
+NSString * const kSTARIO10ErrorTypeIllegalDeviceState = @"IllegalDeviceState";
+NSString * const kSTARIO10ErrorTypeInUse = @"InUse";
+NSString * const kSTARIO10ErrorTypeInvalidOperation = @"InvalidOperation";
+NSString * const kSTARIO10ErrorTypeNotFound = @"NotFound";
+NSString * const kSTARIO10ErrorTypeUnprintable = @"Unprintable";
+NSString * const kSTARIO10ErrorTypeUnsupportedModel = @"UnsupportedModel";
+NSString 

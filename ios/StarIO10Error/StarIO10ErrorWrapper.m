32f8aa06c03357cb03df875440b2aedd75702c9d
nozhangco <91894496+nozhangco@users.noreply.github.com>
2020-09-13 21:00:56 +0000

    Refactoring pipelines

--- ios/StarIO10Error/StarIO10ErrorWrapper.m
+++ ios/StarIO10Error/StarIO10ErrorWrapper.m
@@ -22,4 +22,35 @@ NSString * const kSTARIO10ErrorTypeInvalidOperation = @"InvalidOperation";
 NSString * const kSTARIO10ErrorTypeNotFound = @"NotFound";
 NSString * const kSTARIO10ErrorTypeUnprintable = @"Unprintable";
 NSString * const kSTARIO10ErrorTypeUnsupportedModel = @"UnsupportedModel";
-NSString 
+NSString * const kSTARIO10ErrorTypeUnknown = @"Unknown";
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        _objManager = StarObjectManager.sharedManager;
+    }
+    return self;
+}
+
++ (BOOL)requiresMainQueueSetup
+{
+    return NO;
+}
+
+RCT_EXPORT_MODULE()
+
+RCT_REMAP_METHOD(dispose,
+                 disposeWithObjectIdentifier:(nonnull NSString *)objID
+                 resolver:(RCTPromiseResolveBlock)resolve
+                 rejecter:(RCTPromiseRejectBlock)reject)
+{
+    [_objManager remove:objID];
+
+    resolve(nil);
+}
+
+RCT_REMAP_METHOD(getType,
+                 getTypeWithObjectIdentifier:(nonnull NSString *)objID
+                 resolver:(RCTPromiseResolveBlock)resolve
+                 rej

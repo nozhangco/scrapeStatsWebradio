f82ef370d18679440534c6b176bde699a9a6fb33
nozhangco <91894496+nozhangco@users.noreply.github.com>
2020-09-13 06:11:14 +0000

    cp README.md docs/_includes/README.md for GitHub Page

---
+++ index.d.ts
@@ -0,0 +1,12 @@
+import * as StarXpandCommand from './src/StarXpandCommand/StarXpandCommand';
+export { StarXpandCommand };
+export { DisplayDelegate } from './src/DisplayDelegate';
+export { InputDeviceDelegate } from './src/InputDeviceDelegate';
+export { InterfaceType } from './src/InterfaceType';
+export { PrinterDelegate } from './src/PrinterDelegate';
+export { StarDeviceDiscoveryManager } from './src/StarDeviceDiscoveryManager';
+export { StarDeviceDiscoveryManagerFactory } from './src/StarDeviceDiscoveryManagerFactory';
+export { StarConnectionSettings } from './src/StarConnectionSettings';
+export { StarPrinterModel } from './src/StarPrinterModel';
+export { StarPrinterEmulation } from './src/StarPrinterEmulation';
+export { StarPrinter } from './src/StarPr

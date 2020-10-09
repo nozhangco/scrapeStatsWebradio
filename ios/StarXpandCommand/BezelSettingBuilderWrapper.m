11926bbbc6c95714c55baeb4a72adee8383a3fda
nozhangco <91894496+nozhangco@users.noreply.github.com>
2020-10-09 07:27:11 +0000

    updates to the readme

--- ios/StarXpandCommand/BezelSettingBuilderWrapper.m
+++ ios/StarXpandCommand/BezelSettingBuilderWrapper.m
@@ -32,4 +32,30 @@ RCT_REMAP_METHOD(init,
                  createWithResolver:(RCTPromiseResolveBlock)resolve
                  rejecter:(RCTPromiseRejectBlock)reject)
 {
-    STARIO10StarXpandCommandBezelSettingBuilder *builder = [[STARIO10StarXpan
+    STARIO10StarXpandCommandBezelSettingBuilder *builder = [[STARIO10StarXpandCommandBezelSettingBuilder alloc] init];
+    
+    if (builder == nil) {
+        reject(@"Error", @"Fail to create object.", nil);
+        return;
+    }
+    
+    NSString *objID = [_objManager add:builder];
+    
+    resolve(objID);
+}
+
+RCT_REMAP_METHOD(dispose,
+                 disposeWithObjectIdentifier:(nonnull NSString *)objID
+                 resolver:(RCTPromiseResolveBlock)resolve
+                 rejecter:(RCTPromiseRejectBlock)reject)
+{
+    [_objManager remove:objID];
+    
+    resolve(nil);
+}
+
+RCT_REMAP_METHOD(settingAutomaticPageLength,
+                 settingAutomaticPageLengthWithObjectIdentifier:(nonnull NSString *)objID
+                 enable:(BOOL)enable
+                 resolver:(RCTPromiseResolveBlock)resolve
+                 rejecter:(

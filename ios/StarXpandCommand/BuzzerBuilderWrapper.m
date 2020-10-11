f85c5896cee77a39dd705592a3a2ffab8b606956
nozhangco <91894496+nozhangco@users.noreply.github.com>
2020-10-11 09:09:57 +0000

    Update startup.sh

--- ios/StarXpandCommand/BuzzerBuilderWrapper.m
+++ ios/StarXpandCommand/BuzzerBuilderWrapper.m
@@ -34,4 +34,31 @@ RCT_REMAP_METHOD(init,
                  createWithResolver:(RCTPromiseResolveBlock)resolve
                  rejecter:(RCTPromiseRejectBlock)reject)
 {
-    STARIO10StarXpandCommandBuzzerBuilder *builder = [[STARIO1
+    STARIO10StarXpandCommandBuzzerBuilder *builder = [[STARIO10StarXpandCommandBuzzerBuilder alloc] init];
+    
+    if (builder == nil) {
+        reject(@"Error", @"Fail to create object.", nil);
+        return;
+    }
+    
+    NSString *objID = [_objManager add:builder];
+    
+    resolve(objID);
+}
+
+RCT_REMAP_METHOD(dispose,
+                 disposeWithObjectIdentifier:(nonnull NSString *)objID
+                 resolver:(RCTPromiseResolveBlock)resolve
+                 rejecter:(RCTPromiseRejectBlock)reject)
+{
+    [_objManager remove:objID];
+    
+    resolve(nil);
+}
+
+RCT_REMAP_METHOD(actionDrive,
+                 actionDriveWithObjectIdentifier:(nonnull NSString *)objID
+                 channel:(nonnull NSString *)channel
+                 repeat:(nonnull NSNumber *)repeat
+                 onTime:(nonnull NSNumber *)onTime
+         

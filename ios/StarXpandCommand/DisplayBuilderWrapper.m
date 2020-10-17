380ada69fcd8025cb363146d1a86a68eff870b2a
nozhangco <91894496+nozhangco@users.noreply.github.com>
2020-10-17 19:55:30 +0000

    clarifying comment

--- ios/StarXpandCommand/DisplayBuilderWrapper.m
+++ ios/StarXpandCommand/DisplayBuilderWrapper.m
@@ -4,3 +4,42 @@
 #import <React/RCTLog.h>
 
 @import StarIO10;
+
+@interface DisplayBuilderWrapper()
+
+@property(assign, nonatomic) StarObjectManager *objManager;
+
+@end
+
+@implementation DisplayBuilderWrapper
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        _objManager = StarObjectManager.sharedManager;
+    }
+    return self;
+}
+
++ (BOOL)requiresMainQueueSetup
+{
+    return NO;
+}
+
+RCT_EXPORT_MODULE()
+
+RCT_REMAP_METHOD(init,
+                 createWithResolver:(RCTPromiseResolveBlock)resolve
+                 rejecter:(RCTPromiseRejectBlock)reject)
+{
+    STARIO10StarXpandCommandDisplayBuilder *builder = [[STARIO10StarXpandCommandDisplayBuilder alloc] init];
+    
+    if (builder == nil) {
+        reject(@"Error", @"Fail to create object.", nil);
+        return;
+    }
+    
+    NSString *objID = [_objManager add:builder];
+    
+    resolv

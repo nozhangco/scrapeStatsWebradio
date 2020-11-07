d851aeea3685b7c48cc2a08c0bae72c1af93c59d
nozhangco <91894496+nozhangco@users.noreply.github.com>
2020-11-07 08:35:16 +0000

    Pontoon: Update Hungarian (hu) localization of Mozilla Advocacy

--- ios/StarXpandCommand/DrawerBuilderWrapper.m
+++ ios/StarXpandCommand/DrawerBuilderWrapper.m
@@ -33,4 +33,30 @@ RCT_REMAP_METHOD(init,
                  createWithResolver:(RCTPromiseResolveBlock)resolve
                  rejecter:(RCTPromiseRejectBlock)reject)
 {
-    STARIO10StarXpandCommandDrawerBuilder *builder = [[STARIO
+    STARIO10StarXpandCommandDrawerBuilder *builder = [[STARIO10StarXpandCommandDrawerBuilder alloc] init];
+    
+    if (builder == nil) {
+        reject(@"Error", @"Fail to create object.", nil);
+        return;
+    }
+    
+    NSString *objID = [_objManager add:builder];
+    
+    resolve(objID);
+}
+
+RCT_REMAP_METHOD(dispose,
+                 disposeWithObjectIdentifier:(nonnull NSString *)objID
+                 resolver:(RCTPromiseResolveBlock)resolve
+                 rejecter:(RCTPromiseRejectBlock)reject)
+{
+    [_objManager remove:objID];
+    
+    resolve(nil);
+}
+
+RCT_REMAP_METHOD(actionOpen,
+                 actionOpenWithObjectIdentifier:(nonnull NSString *)objID
+                 channel:(nonnull NSString *)channel
+                 onTime:(nonnull NSNumber *)onTime
+                 resolver:

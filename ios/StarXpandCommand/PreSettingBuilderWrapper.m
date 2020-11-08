495b73746d2d33c1676a7c4d05afac606888e103
nozhangco <91894496+nozhangco@users.noreply.github.com>
2020-11-08 06:28:30 +0000

    Post deleted:

--- ios/StarXpandCommand/PreSettingBuilderWrapper.m
+++ ios/StarXpandCommand/PreSettingBuilderWrapper.m
@@ -32,3 +32,29 @@ RCT_EXPORT_MODULE()
 RCT_REMAP_METHOD(init,
                  createWithResolver:(RCTPromiseResolveBlock)resolve
                  rejecter:(RCTPromiseRejectBlock)reject)
+{
+    STARIO10StarXpandCommandPreSettingBuilder *builder = [[STARIO10StarXpandCommandPreSettingBuilder alloc] init];
+    
+    if (builder == nil) {
+        reject(@"Error", @"Fail to create object.", nil);
+        return;
+    }
+
+    NSString *objID = [_objManager add:builder];
+    
+    resolve(objID);
+}
+
+RCT_REMAP_METHOD(dispose,
+                 disposeWithObjectIdentifier:(nonnull NSString *)objID
+                 resolver:(RCTPromiseResolveBlock)resolve
+                 rejecter:(RCTPromiseRejectBlock)reject)
+{
+    [_objManager remove:objID];
+    
+    resolve(nil);
+}
+
+RCT_REMAP_METHOD(addPresenterSetting,
+                 addPresenterSettingWithObjectIdentifier:(nonnull NSString *)objID
+                 presenterSettingBuilderIdentifie

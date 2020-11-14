481bfa0fe1ac14ba6703b2b0cc8a68b529670a2f
nozhangco <91894496+nozhangco@users.noreply.github.com>
2020-11-14 21:36:09 +0000

    Stub

--- ios/StarXpandCommand/StarXpandCommandBuilderWrapper.m
+++ ios/StarXpandCommand/StarXpandCommandBuilderWrapper.m
@@ -29,4 +29,31 @@
 RCT_EXPORT_MODULE()
 
 RCT_REMAP_METHOD(init,
-                 createWithResolver:(RCTPro
+                 createWithResolver:(RCTPromiseResolveBlock)resolve
+                 rejecter:(RCTPromiseRejectBlock)reject)
+{
+    STARIO10StarXpandCommandBuilder *builder = [[STARIO10StarXpandCommandBuilder alloc] init];
+    
+    if (builder == nil) {
+        reject(@"Error", @"Fail to create object.", nil);
+        return;
+    }
+    
+    NSString *objID = [_objManager add:builder];
+    
+    resolve(objID);
+}
+
+RCT_REMAP_METHOD(dispose,
+                 disposeWithObjectIdentifier:(nonnull NSString *)objID
+                 resolver:(RCTPromiseResolveBlock)resolve
+                 rejecter:(RCTPromiseRejectBlock)reject)
+{
+    [_objManager remove:objID];
+    
+    resolve(nil);
+}
+
+RCT_REMAP_METHOD(setPreSetting,
+                 setPreSettingWithObjectIdentifier:(nonnull NSString *)objID
+                 preSettingBuilderIden

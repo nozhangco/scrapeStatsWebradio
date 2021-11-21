22d5f7b28526e0814c005a4be28301b8eb8d0c59
nozhangco <91894496+nozhangco@users.noreply.github.com>
2021-11-21 14:00:25 +0000

    fix wrong comment

--- ios/libs/StarIO10.xcframework/ios-arm64/StarIO10.framework/PrivateHeaders/StarXpandCommandParserWrapper.h
+++ ios/libs/StarIO10.xcframework/ios-arm64/StarIO10.framework/PrivateHeaders/StarXpandCommandParserWrapper.h
@@ -5,4 +5,15 @@
 //  Copyright 2021 Star Micronics Co., Ltd. All rights reserved.
 //
 
-#import <Foundation/Fou
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface StarXpandCommandParserWrapper : NSObject
+
++ (nullable NSData *)parseWithCommand:(NSString *)command
+                            emulation:(NSString *)emulation
+                                model:(NSString *)model;
+@end
+
+NS_ASSUME_NONNULL_END

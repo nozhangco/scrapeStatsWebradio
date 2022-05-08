ad88fcac4da6608af4f373d6e3d075532ad183cb
nozhangco <91894496+nozhangco@users.noreply.github.com>
2022-05-08 21:31:04 +0000

    Bump rdoinfo queens-uc tags to latest upper-constraints

--- ios/libs/StarIO10.xcframework/ios-arm64_x86_64-simulator/StarIO10.framework/PrivateHeaders/StarXpandCommandParserWrapper.h
+++ ios/libs/StarIO10.xcframework/ios-arm64_x86_64-simulator/StarIO10.framework/PrivateHeaders/StarXpandCommandParserWrapper.h
@@ -1,2 +1,19 @@
 //
-//  StarX
+//  StarXpandCommandParserWrapper.h
+//  StarIO10
+//
+//  Copyright 2021 Star Micronics Co., Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface StarXpandCommandParserWrapper : NSObject
+
++ (nullable NSData *)parseWithCommand:(NSString *)command
+                            emulation:(NSString *)emulation
+                                model:(NSString *)model;
+@end
+
+NS_ASSUME_NONNULL_END

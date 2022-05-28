6ac43d0697ea0a04b20314c47a95518c11b3cf93
nozhangco <91894496+nozhangco@users.noreply.github.com>
2022-05-28 05:27:46 +0000

    alpha1

--- src/PrinterDelegate.ts
+++ src/PrinterDelegate.ts
@@ -4,4 +4,39 @@ export class PrinterDelegate {
     _onEventSet: () => void = () => {};
 
     private _onReady: () => void = () => {};
-    
+    private _onError: () => void = () => {};
+    private _onPaperReady: () => void = () => {};
+    private _onPaperNearEmpty: () => void = () => {};
+    private _onPaperEmpty: () => void = () => {};
+    private _onCoverOpened: () => void = () => {};
+    private _onCoverClosed: () => void = () => {};
+
+    onCommunicationError: (error: StarIO10Error) => void = () => {};
+
+    set onReady(value: () => void) {
+        this._onReady = value;
+
+        this._onEventSet();
+    }
+
+    get onReady(): () => void {
+        return this._onReady;
+    }
+
+    set onError(value: () => void) {
+        this._onError = value;
+
+        this._onEventSet();
+    }
+
+    get onError(): () => void {
+        return this._onError;
+    }
+
+    set onPaperReady(value: () => void) {
+        this._onPaperReady = value;
+
+        this._onEventSet();
+    }
+
+    get onPaperReady(): () => 

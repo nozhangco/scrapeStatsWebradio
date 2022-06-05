c418f5a6f32c132b058bb2158dbdccf71348ae8b
nozhangco <91894496+nozhangco@users.noreply.github.com>
2022-06-05 10:15:59 +0000

    Fixing some stuff

--- src/StarIO10ErrorFactory.ts
+++ src/StarIO10ErrorFactory.ts
@@ -1,2 +1,12 @@
 import { NativeModules } from 'react-native';
-import { StarIO10ArgumentError } from './StarIO10Argum
+import { StarIO10ArgumentError } from './StarIO10ArgumentError';
+import { StarIO10BadResponseError } from './StarIO10BadResponseError';
+import { StarIO10CommunicationError } from './StarIO10CommunicationError';
+import { StarIO10IllegalDeviceStateError } from './StarIO10IllegalDeviceStateError';
+import { StarIO10InUseError } from './StarIO10InUseError';
+import { StarIO10InvalidOperationError } from './StarIO10InvalidOperationError';
+import { StarIO10NotFoundError } from './StarIO10NotFoundError';
+import { StarIO10UnknownError } from './StarIO10UnknownError';
+import { StarIO10UnprintableError } from './StarIO10UnprintableError';
+import { StarIO10UnsupportedModelError } from './StarIO10UnsupportedModelError';
+import { StarPrinterStatusFactory } from 

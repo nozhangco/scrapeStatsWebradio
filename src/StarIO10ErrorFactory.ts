c0e8f38dec0e61d12c61664103ebee83c95a2e30
nozhangco <91894496+nozhangco@users.noreply.github.com>
2022-06-05 15:06:13 +0000

    Updates package-lock (patch)

--- src/StarIO10ErrorFactory.ts
+++ src/StarIO10ErrorFactory.ts
@@ -9,4 +9,19 @@ import { StarIO10NotFoundError } from './StarIO10NotFoundError';
 import { StarIO10UnknownError } from './StarIO10UnknownError';
 import { StarIO10UnprintableError } from './StarIO10UnprintableError';
 import { StarIO10UnsupportedModelError } from './StarIO10UnsupportedModelError';
-import { StarPrinterStatusFactory } from 
+import { StarPrinterStatusFactory } from './StarPrinterStatusFactory';
+import { StarIO10Error } from './StarIO10Error';
+import { StarPrinterStatus } from './StarPrinterStatus';
+
+export class StarIO10ErrorFactory {
+    static async create(identifier: String): Promise<StarIO10Error> {
+        var error;
+
+        try {
+            var type = await NativeModules.StarIO10ErrorWrapper.getType(identifier);
+            var message = await NativeModules.StarIO10ErrorWrapper.getMessage(identifier);
+            var errorCode = await NativeModules.StarIO10ErrorWrapper.getErrorCode(identifier);
+            error = await StarIO10ErrorFactory._buildObject(identifier, type, message, errorCode);
+        }
+        catch(_) {
+            error = new StarIO10UnknownError("Failed to create Error.");

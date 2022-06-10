30f5dbfd92ce9032c274cc324d4d58d07d8d629f
nozhangco <91894496+nozhangco@users.noreply.github.com>
2022-06-10 21:20:33 +0000

    Updated Dictionary.

--- src/StarIO10Logger.ts
+++ src/StarIO10Logger.ts
@@ -6,4 +6,31 @@ import { NativeObject } from './NativeObject';
 import { StarIO10ErrorFactory } from './StarIO10ErrorFactory';
 import { version } from '../package.json'
 
-export c
+export class StarIO10Logger extends NativeObject {
+    private static _instance : StarIO10Logger;
+    private _sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+    private constructor() {
+        super();
+    }
+
+    public static get instance() : StarIO10Logger {
+        if (this._instance == null) {
+            this._instance = new StarIO10Logger();
+        }
+
+        return this._instance;
+    }
+
+    async start(): Promise<void> {
+        await this._initNativeObject();
+
+        await NativeModules.StarIO10LoggerWrapper.start()
+        .catch(async (nativeError: any) => {
+            var error = await StarIO10ErrorFactory.create(nativeError.code);
+            throw error;
+        });
+    }
+
+    async stop(): Promise<void> {
+        await this._initNativeO

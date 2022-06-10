e580b19c876404b40df0c5f942c1bf768ffdedb7
nozhangco <91894496+nozhangco@users.noreply.github.com>
2022-06-10 08:57:51 +0000

    HID: usbhid: fix recursive deadlock

---
+++ src/StarIO10Logger.ts
@@ -0,0 +1,9 @@
+import {
+    NativeModules
+} from 'react-native';
+
+import { NativeObject } from './NativeObject';
+import { StarIO10ErrorFactory } from './StarIO10ErrorFactory';
+import { version } from '../package.json'
+
+export c

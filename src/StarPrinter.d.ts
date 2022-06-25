257f2b542fc0df39ed417d49a9692f0f7aad7351
nozhangco <91894496+nozhangco@users.noreply.github.com>
2022-06-25 13:05:18 +0000

    Small readme changes

--- src/StarPrinter.d.ts
+++ src/StarPrinter.d.ts
@@ -6,4 +6,23 @@ import { PrinterDelegate } from './PrinterDelegate';
 import { DrawerDelegate } from './DrawerDelegate';
 import { InputDeviceDelegate } from './InputDeviceDelegate';
 import { DisplayDelegate } from './DisplayDelegate';
-export 
+export declare class StarPrinter extends NativeObject {
+    private _eventSubscriptions;
+    private _connectionSettings;
+    private _printerDelegate;
+    private _drawerDelegate;
+    private _inputDeviceDelegate;
+    private _displayDelegate;
+    _information: StarPrinterInformation | undefined;
+    openTimeout: number;
+    printTimeout: number;
+    getStatusTimeout: number;
+    get information(): StarPrinterInformation | undefined;
+    get connectionSettings(): StarConnectionSettings;
+    get printerDelegate(): PrinterDelegate;
+    get drawerDelegate(): DrawerDelegate;
+    get inputDeviceDelegate(): InputDeviceDelegate;
+    get displayDelegate(): DisplayDelegate;
+    constructor(connectionSettings: StarConnectionSettings);
+    open(): Promise<void>;
+    print(command

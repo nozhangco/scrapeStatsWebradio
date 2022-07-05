b1ea14c30d7b088788254ed083e723291f874685
nozhangco <91894496+nozhangco@users.noreply.github.com>
2022-07-05 17:59:03 +0000

    Code style fix

--- src/StarXpandCommand/BaseStarXpandCommandBuilder.d.ts
+++ src/StarXpandCommand/BaseStarXpandCommandBuilder.d.ts
@@ -5,4 +5,7 @@ export declare abstract class BaseStarXpandCommandBuilder extends NativeObject {
     protected _initAllNativeObjects(): Promise<void>;
     protected _disposeAllNativeObjects(): Promise<void>;
     protected _addChild(builder: BaseStarXpandCommandBuilder): void;
- 
+    protected _removeChild(builder: BaseStarXpandCommandBuilder): void;
+    protected _addAction(action: () => Promise<void>): void;
+    protected _executeAllActions(): Promise<void>;
+}

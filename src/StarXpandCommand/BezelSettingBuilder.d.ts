bd7c31c2a32899f2967c0c5318f0b521849130a6
nozhangco <91894496+nozhangco@users.noreply.github.com>
2022-07-14 10:55:45 +0000

    added devise

--- src/StarXpandCommand/BezelSettingBuilder.d.ts
+++ src/StarXpandCommand/BezelSettingBuilder.d.ts
@@ -1,3 +1,8 @@
 import { BaseStarXpandCommandBuilder } from './BaseStarXpandCommandBuilder';
 import { StarXpandCommand } from '../../index';
-export declar
+export declare class BezelSettingBuilder extends BaseStarXpandCommandBuilder {
+    settingAutomaticPageLength(enable: boolean): BezelSettingBuilder;
+    settingLedAutomaticBlink(parameter: StarXpandCommand.Bezel.LedAutomaticBlinkParameter): BezelSettingBuilder;
+    protected _initNativeObjectImpl(): Promise<string>;
+    protected _disposeNativeObjectImpl(nativeObject: string): Promise<void>;
+}
---
+++ src/StarXpandCommand/Buzzer/DriveParameter.d.ts
@@ -0,0 +1,5 @@
+import { Channel } from './Channel';
+export declare class DriveParameter {
+    private _channel;
+    private _repeat;
+ 

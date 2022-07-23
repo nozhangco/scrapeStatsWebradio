84e052a238a57da1f2821741a328c1b2e62c7d05
nozhangco <91894496+nozhangco@users.noreply.github.com>
2022-07-23 22:32:13 +0000

    Add Changelog

--- src/StarXpandCommand/DisplayBuilder.ts
+++ src/StarXpandCommand/DisplayBuilder.ts
@@ -4,4 +4,21 @@ import { StarIO10ErrorFactory } from '../StarIO10ErrorFactory';
 import { StarXpandCommand } from '../../index';
 
 export class DisplayBuilder extends BaseStarXpandCommandBuilder {
-    styleInternationalCharacter(type: StarXpandCommand.Display.Internationa
+    styleInternationalCharacter(type: StarXpandCommand.Display.InternationalCharacterType): DisplayBuilder {
+        this._addAction(async() => {
+            await NativeModules.DisplayBuilderWrapper.styleInternationalCharacter(this._nativeObject, type)
+            .catch(async (nativeError: any) => {
+                var error = await StarIO10ErrorFactory.create(nativeError.code);
+                throw error;
+            });
+        });
+
+        return this;
+    }
+
+    styleCharacterEncoding(type: StarXpandCommand.Display.CharacterEncodingType): DisplayBuilder {
+        this._addAction(async() => {
+            await NativeModules.DisplayBuilderWrapper.styleCharacterEncoding(this._nativeObject, type)
+            .catch(async (nativeError: any) => {
+                var error = await StarIO10ErrorFactory.create(nativeError.code);
+                t

3f934f4a0d2932cb220fc9afb0202bd89fd5e56d
nozhangco <91894496+nozhangco@users.noreply.github.com>
2022-08-11 19:16:08 +0000

    Resolve lint errors

--- src/StarXpandCommand/DocumentBuilder.ts
+++ src/StarXpandCommand/DocumentBuilder.ts
@@ -3,4 +3,20 @@ import { BaseStarXpandCommandBuilder } from './BaseStarXpandCommandBuilder';
 import { StarIO10ErrorFactory } from '../StarIO10ErrorFactory';
 import { StarXpandCommand } from '../../index';
 
-expor
+export class DocumentBuilder extends BaseStarXpandCommandBuilder {
+    settingTopMargin(height: number): DocumentBuilder {
+        this._addAction(async() => {
+            await NativeModules.DocumentBuilderWrapper.settingTopMargin(this._nativeObject, height)
+            .catch(async (nativeError: any) => {
+                var error = await StarIO10ErrorFactory.create(nativeError.code);
+                throw error;
+            });
+        });
+
+        return this;
+    }
+
+    settingBlackMark(parameter: StarXpandCommand.Printer.BlackMarkParameter): DocumentBuilder {
+        this._addAction(async() => {
+            await NativeModules.DocumentBuilderWrapper.settingBlackMark(this._nativeObject, parameter.enable, parameter.position)
+            .catch(async (nativeError: any) 

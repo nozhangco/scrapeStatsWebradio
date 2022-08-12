515d50312cd147de9a0758eccb17b4be873a7f70
nozhangco <91894496+nozhangco@users.noreply.github.com>
2022-08-12 11:29:34 +0000

    .travis.yml

--- src/StarXpandCommand/DocumentBuilder.ts
+++ src/StarXpandCommand/DocumentBuilder.ts
@@ -19,4 +19,27 @@ export class DocumentBuilder extends BaseStarXpandCommandBuilder {
     settingBlackMark(parameter: StarXpandCommand.Printer.BlackMarkParameter): DocumentBuilder {
         this._addAction(async() => {
             await NativeModules.DocumentBuilderWrapper.settingBlackMark(this._nativeObject, parameter.enable, parameter.position)
-            .catch(async (nativeError: any) 
+            .catch(async (nativeError: any) => {
+                var error = await StarIO10ErrorFactory.create(nativeError.code);
+                throw error;
+            });
+        });
+
+        return this;
+    }
+
+    settingLabel(parameter: StarXpandCommand.Printer.LabelParameter): DocumentBuilder {
+        this._addAction(async() => {
+            await NativeModules.DocumentBuilderWrapper.settingLabel(this._nativeObject, parameter.enable)
+            .catch(async (nativeError: any) => {
+                var error = await StarIO10ErrorFactory.create(nativeError.code);
+                throw error;
+            });
+        });
+
+        return this;
+    }
+
+    settingHoldPrint(parameter: StarXpandCommand.Printer.HoldPrintParameter): DocumentBuilder {
+        this._addAction(async() => {
+  

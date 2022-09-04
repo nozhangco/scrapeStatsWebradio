d0622f946857464161bb4d09859ffec165f68be4
nozhangco <91894496+nozhangco@users.noreply.github.com>
2022-09-04 11:37:26 +0000

    Updated nuspec

--- src/StarXpandCommand/Printer/BarcodeParameter.ts
+++ src/StarXpandCommand/Printer/BarcodeParameter.ts
@@ -6,4 +6,39 @@ export class BarcodeParameter {
     private _symbology: BarcodeSymbology;
     private _printHri: boolean = false;
     private _barDots: number = 2;
-    private _barRatioLevel: BarcodeBar
+    private _barRatioLevel: BarcodeBarRatioLevel = BarcodeBarRatioLevel.Level0;
+    private _height: number = 10;
+
+    get content(): string {
+        return this._content;
+    }
+
+    get symbology(): BarcodeSymbology {
+        return this._symbology;
+    }
+
+    get printHri(): boolean {
+        return this._printHri;
+    }
+
+    get barDots(): number {
+        return this._barDots;
+    }
+
+    get barRatioLevel(): BarcodeBarRatioLevel {
+        return this._barRatioLevel;
+    }
+
+    get height(): number {
+        return this._height;
+    }
+
+    constructor(content: string, symbology: BarcodeSymbology) {
+        this._content = content;
+        this._symbology = symbology;
+    }
+
+    setPrintHri(printHri: boolean): BarcodeParameter {
+        this._printHri = printHri;
+
+        return this;

e9bb31f7585dd7051f48e9c5369116e49143d926
nozhangco <91894496+nozhangco@users.noreply.github.com>
2022-09-22 22:13:35 +0000

    fix config file

--- src/StarXpandCommand/PrinterBuilder.d.ts
+++ src/StarXpandCommand/PrinterBuilder.d.ts
@@ -5,4 +5,14 @@ export declare class PrinterBuilder extends BaseStarXpandCommandBuilder {
     styleFont(type: StarXpandCommand.Printer.FontType): PrinterBuilder;
     styleBold(enable: boolean): PrinterBuilder;
     styleInvert(enable: boolean): PrinterBuilder;
-    styleUnderLi
+    styleUnderLine(enable: boolean): PrinterBuilder;
+    styleMagnification(parameter: StarXpandCommand.MagnificationParameter): PrinterBuilder;
+    styleCharacterSpace(width: number): PrinterBuilder;
+    styleLineSpace(height: number): PrinterBuilder;
+    styleHorizontalPositionTo(position: number): PrinterBuilder;
+    styleHorizontalPositionBy(position: number): PrinterBuilder;
+    styleHorizontalTabPositions(positions: Array<number>): PrinterBuilder;
+    styleInternationalCharacter(type: StarXpandCommand.Printer.InternationalCharacterType): PrinterBuilder;
+    styleSecondPriorityCharacterEncoding(type: StarXpandCommand.Printer.CharacterEncodingType): PrinterBuilder;
+    styleCjkCharacterPriority(types: Array<StarXpandCommand.Printer.CjkCharacterType>): PrinterBuilder;
+    actionCut(type: StarXp

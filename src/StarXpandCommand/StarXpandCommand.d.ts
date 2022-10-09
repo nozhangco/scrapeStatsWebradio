e0eee183776479320054eeb97b2049e5089c71b6
nozhangco <91894496+nozhangco@users.noreply.github.com>
2022-10-09 11:28:03 +0000

    Update frontpage.html

--- src/StarXpandCommand/StarXpandCommand.d.ts
+++ src/StarXpandCommand/StarXpandCommand.d.ts
@@ -6,4 +6,14 @@ import * as Presenter from './Presenter/Presenter';
 import * as Display from './Display/Display';
 import * as Bezel from './Bezel/Bezel';
 export { Printer, Buzzer, Drawer, MelodySpeaker, Presenter, Display, Bezel };
-export * from './StarX
+export * from './StarXpandCommandBuilder';
+export * from './DocumentBuilder';
+export * from './PreSettingBuilder';
+export * from './PresenterSettingBuilder';
+export * from './BezelSettingBuilder';
+export * from './PrinterBuilder';
+export * from './DrawerBuilder';
+export * from './BuzzerBuilder';
+export * from './MelodySpeakerBuilder';
+export * from './DisplayBuilder';
+export * from './MagnificationParameter';
---
+++ src/StarXpandCommand/StarXpandCommand.ts
@@ -0,0 +1,12 @@
+import * as Printer from './Printer/Printer';
+import * as Buzzer from './Buzzer/Buzzer';
+import * as Drawer from './Drawer/Drawer';
+import * as MelodySpeaker from './MelodySpeaker/MelodySpeaker';
+import * as Presenter from './Presenter/Presenter';
+import * as Display from './Display/Display';
+import * as Bezel from './Bezel/Bezel';
+
+export {
+    Printer,
+    Buzzer,
+    Drawer

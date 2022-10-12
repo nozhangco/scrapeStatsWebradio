78c5fb6f45b3f11bb628e965ac5805e24ac6cf82
nozhangco <91894496+nozhangco@users.noreply.github.com>
2022-10-12 18:31:42 +0000

    Fix release target.

--- src/StarXpandCommand/StarXpandCommandBuilder.ts
+++ src/StarXpandCommand/StarXpandCommandBuilder.ts
@@ -4,4 +4,29 @@ import { StarIO10ErrorFactory } from '../StarIO10ErrorFactory';
 import { StarXpandCommand } from '../../index';
 
 export class StarXpandCommandBuilder extends BaseStarXpandCommandBuilder {
-    private _preSetting?: StarXpandCommand.PreSettingB
+    private _preSetting?: StarXpandCommand.PreSettingBuilder;
+
+    set preSetting(preSetting: StarXpandCommand.PreSettingBuilder | undefined) {
+        if(this._preSetting != null) {
+            this._removeChild(this._preSetting);
+        }
+
+        if (preSetting != null) {
+            this._addChild(preSetting);
+        }
+
+        this._preSetting = preSetting;
+    }
+
+    get preSetting(): StarXpandCommand.PreSettingBuilder | undefined {
+        return this._preSetting;
+    }
+
+    constructor() {
+        super();
+
+        this._addAction(async() => {
+            if(this.preSetting != null) {
+                await NativeModules.StarXpandCommandBuilderWrapper.setPreSetting(this._nativeObject, this.preSetting._nativeObject)
+                .catch(async (nativeError: any) => {
+                    va

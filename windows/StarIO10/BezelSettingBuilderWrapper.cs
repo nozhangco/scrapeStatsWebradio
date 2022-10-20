be819b8fbcdcf007e45383005856262abf506f2b
nozhangco <91894496+nozhangco@users.noreply.github.com>
2022-10-20 12:48:53 +0000

    Sync deps

--- windows/StarIO10/BezelSettingBuilderWrapper.cs
+++ windows/StarIO10/BezelSettingBuilderWrapper.cs
@@ -2,4 +2,28 @@
 using StarMicronics.StarIO10.StarXpandCommand;
 using StarMicronics.StarIO10.StarXpandCommand.Bezel;
 
-namesp
+namespace StarMicronics.ReactNative.StarIO10
+{
+    [ReactModule]
+    class BezelSettingBuilderWrapper : StarIO10ObjectWrapper<BezelSettingBuilder>
+    {
+        [ReactMethod("init")]
+        public void Init(IReactPromise<string> promise)
+        {
+            BezelSettingBuilder nativeObject = new BezelSettingBuilder();
+
+            SetObject(nativeObject, out string objectIdentifier);
+
+            promise.Resolve(objectIdentifier);
+        }
+
+        [ReactMethod("dispose")]
+        public void Dispose(string objectIdentifier, IReactPromise<JSValue.Void> promise)
+        {
+            DisposeObject(objectIdentifier);
+
+            promise.Resolve();
+        }
+
+        [ReactMethod("settingAutomaticPageLength")]
+        public void SettingAutomaticPageLength(string objectIdentifier, bool enable, IReact

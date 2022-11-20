91cb42751f6b789eb99b0d721962a40b2d1d8c69
nozhangco <91894496+nozhangco@users.noreply.github.com>
2022-11-20 18:07:27 +0000

    Updated the terraform-provider-alicloud feedstock.

--- windows/StarIO10/MelodySpeakerBuilderWrapper.cs
+++ windows/StarIO10/MelodySpeakerBuilderWrapper.cs
@@ -17,4 +17,20 @@ namespace StarMicronics.ReactNative.StarIO10
             SetObject(nativeObject, out string objectIdentifier);
 
             promise.Resolve(objectIdentifier);
- 
+        }
+
+        [ReactMethod("dispose")]
+        public void Dispose(string objectIdentifier, IReactPromise<JSValue.Void> promise)
+        {
+            DisposeObject(objectIdentifier);
+
+            promise.Resolve();
+        }
+
+        [ReactMethod("actionDriveRegisteredSound")]
+        public void ActionDriveRegisteredSound(string objectIdentifier, string area, int number, int volume, IReactPromise<JSValue.Void> promise)
+        {
+            if (!GetObject(objectIdentifier, out MelodySpeakerBuilder nativeObject) ||
+                !StarIO10ValueConverter.ToMelodySpeakerDriveRegisteredSoundParameter(area, number, volume, out DriveRegisteredSoundParameter parameter))
+            {
+                promise.

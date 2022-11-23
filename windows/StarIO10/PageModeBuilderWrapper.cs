cb10d5ae4be1b4acdbdfe7fb8439bdc7725bf394
nozhangco <91894496+nozhangco@users.noreply.github.com>
2022-11-23 11:45:37 +0000

    Remove unnecessary deps

--- windows/StarIO10/PageModeBuilderWrapper.cs
+++ windows/StarIO10/PageModeBuilderWrapper.cs
@@ -8,4 +8,27 @@ using System.Collections.Generic;
 namespace StarMicronics.ReactNative.StarIO10
 {
     [ReactModule]
-    class PageModeBuilderWrapper
+    class PageModeBuilderWrapper : StarIO10ObjectWrapper<PageModeBuilder>
+    {
+        [ReactMethod("init")]
+        public void Init(IReactPromise<string> promise)
+        {
+            PageModeBuilder nativeObject = new PageModeBuilder();
+
+            SetObject(nativeObject, out string objectIdentifier);
+
+            promise.Resolve(objectIdentifier);
+        }
+
+        [ReactMethod("dispose")]
+        public void Dispose(string objectIdentifier, IReactPromise<JSValue.Void> promise)
+        {
+            DisposeObject(objectIdentifier);
+
+            promise.Resolve();
+        }
+
+        [ReactMethod("stylePrintDirection")]
+        public void StylePrintDirection(string objectIdentifier, string direction, IReactPromise<JSValue.Void> promise)
+        {
+            if (!GetObject(objectIdentifier, out PageMode

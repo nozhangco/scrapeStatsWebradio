5d2ec7dcc9ce0c9db99634788c5b9b215fe3396f
nozhangco <91894496+nozhangco@users.noreply.github.com>
2022-12-11 16:35:28 +0000

    Archive

--- windows/StarIO10/PrinterBuilderWrapper.cs
+++ windows/StarIO10/PrinterBuilderWrapper.cs
@@ -23,4 +23,25 @@ namespace StarMicronics.ReactNative.StarIO10
         [ReactMethod("dispose")]
         public void Dispose(string objectIdentifier, IReactPromise<JSValue.Void> promise)
         {
-            DisposeObj
+            DisposeObject(objectIdentifier);
+
+            promise.Resolve();
+        }
+
+        [ReactMethod("styleAlignment")]
+        public void StyleAlignment(string objectIdentifier, string alignment, IReactPromise<JSValue.Void> promise)
+        {
+            if (!GetObject(objectIdentifier, out PrinterBuilder nativeObject) ||
+                !StarIO10ValueConverter.ToPrinterAlignment(alignment, out Alignment nativeAlignment))
+            {
+                promise.Reject(new ReactError());
+                return;
+            }
+
+            nativeObject.StyleAlignment(nativeAlignment);
+
+            promise.Resolve();
+        }
+
+        [ReactMethod("addPageMode")]
+        public void AddPageMode(string objectIdentifier, double x, double y, double width, double height, string pageModeBuilderIdentifier, IReactPromise<JSV

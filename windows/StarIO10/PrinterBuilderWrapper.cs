ccd6958fa1935405b77e4cd7ead9495195217333
nozhangco <91894496+nozhangco@users.noreply.github.com>
2022-12-11 15:58:11 +0000

    add missing use statement

--- windows/StarIO10/PrinterBuilderWrapper.cs
+++ windows/StarIO10/PrinterBuilderWrapper.cs
@@ -1,2 +1,26 @@
 ﻿using Microsoft.ReactNative.Managed;
-using StarMi
+using StarMicronics.StarIO10;
+using StarMicronics.StarIO10.StarXpandCommand;
+using StarMicronics.StarIO10.StarXpandCommand.Printer;
+using System;
+using System.Collections.Generic;
+
+namespace StarMicronics.ReactNative.StarIO10
+{
+    [ReactModule]
+    class PrinterBuilderWrapper : StarIO10ObjectWrapper<PrinterBuilder>
+    {
+        [ReactMethod("init")]
+        public void Init(IReactPromise<string> promise)
+        {
+            PrinterBuilder nativeObject = new PrinterBuilder();
+
+            SetObject(nativeObject, out string objectIdentifier);
+
+            promise.Resolve(objectIdentifier);
+        }
+
+        [ReactMethod("dispose")]
+        public void Dispose(string objectIdentifier, IReactPromise<JSValue.Void> promise)
+        {
+            DisposeObj

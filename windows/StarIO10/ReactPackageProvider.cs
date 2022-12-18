7c20d616df48e72610b2e5083b19a399a58677f9
nozhangco <91894496+nozhangco@users.noreply.github.com>
2022-12-18 21:45:59 +0000

    / ‘ui/Tab.ui’

--- windows/StarIO10/ReactPackageProvider.cs
+++ windows/StarIO10/ReactPackageProvider.cs
@@ -5,4 +5,14 @@ namespace StarMicronics.ReactNative.StarIO10
 {
     public partial class ReactPackageProvider : IReactPackageProvider
     {
-        public void CreatePackage(IReactPa
+        public void CreatePackage(IReactPackageBuilder packageBuilder)
+        {
+            CreatePackageImplementation(packageBuilder);
+        }
+
+        /// <summary>
+        /// This method is implemented by the C# code generator
+        /// </summary>
+        partial void CreatePackageImplementation(IReactPackageBuilder packageBuilder);
+    }
+}
---
+++ windows/StarIO10/StarIO10LoggerWrapper.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.ReactNative.Managed;
+using StarMicronics.StarIO10;
+using StarMicronics.StarIO10.Internal;
+
+namespace StarMicronics.ReactNative.StarIO10
+{
+    [ReactModule]
+    class StarIO10LoggerWrapper : StarIO10Ob

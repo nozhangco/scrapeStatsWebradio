5399d1259f876cac586115acbbbb28354e90afc5
nozhangco <91894496+nozhangco@users.noreply.github.com>
2022-12-18 13:44:36 +0000

    truc

--- windows/StarIO10/Properties/AssemblyInfo.cs
+++ windows/StarIO10/Properties/AssemblyInfo.cs
@@ -7,4 +7,23 @@ using System.Runtime.InteropServices;
 // これらの属性値を変更してください。
 [assembly: AssemblyTitle("StarIO10")]
 [assembly: AssemblyDescription("")]
-[assembly: Assembly
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StarIO10")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// アセンブリのバージョン情報は次の 4 つの値で構成されています:
+//
+//      メジャー バージョン
+//      マイナー バージョン
+//      ビルド番号
+//      リビジョン
+//
+// すべての値を指定するか、次を使用してビルド番号とリビジョン番号を既定に設定できます
+// 既定値にすることができます:
+//[アセンブリ: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: ComVisible(false)]
---
+++ windows/StarIO10/ReactPackageProvider.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.ReactNative;
+using Microsoft.ReactNative.Managed;
+
+namespace StarMicronics.ReactNative.StarIO10
+{
+    public partial class ReactPackageProvider : IReactPackageProvider
+    {
+        public void CreatePackage(IReactPa

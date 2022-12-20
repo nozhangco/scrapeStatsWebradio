3f365788a4563057960337450cd6286267fadd7d
nozhangco <91894496+nozhangco@users.noreply.github.com>
2022-12-20 14:01:18 +0000

    Updated Resume

--- windows/StarIO10/StarIO10ValueConverter.cs
+++ windows/StarIO10/StarIO10ValueConverter.cs
@@ -1,3 +1,28 @@
 ﻿using Microsoft.ReactNative.Managed;
 using StarMicronics.StarIO10;
-using StarMicronics.StarIO
+using StarMicronics.StarIO10.StarXpandCommand;
+using StarMicronics.StarIO10.StarXpandCommand.Buzzer;
+using StarMicronics.StarIO10.StarXpandCommand.Display;
+using StarMicronics.StarIO10.StarXpandCommand.Drawer;
+using StarMicronics.StarIO10.StarXpandCommand.MelodySpeaker;
+using StarMicronics.StarIO10.StarXpandCommand.Presenter;
+using StarMicronics.StarIO10.StarXpandCommand.Printer;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel;
+using Windows.Graphics.Imaging;
+using Windows.Storage;
+using Windows.Storage.Streams;
+
+namespace StarMicronics.ReactNative.StarIO10
+{
+    internal class StarIO10ValueConverter
+    {
+   

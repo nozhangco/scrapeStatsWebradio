6802f969aaae652be133623f9ba59d1bae8db0b6
nozhangco <91894496+nozhangco@users.noreply.github.com>
2023-02-12 17:45:56 +0000

    Adding examples.

--- windows/StarIO10/StarXpandCommandBuilderWrapper.cs
+++ windows/StarIO10/StarXpandCommandBuilderWrapper.cs
@@ -5,3 +5,26 @@ namespace StarMicronics.ReactNative.StarIO10
 {
     [ReactModule]
     class StarXpandCommandBuilderWrapper : StarIO10ObjectWrapper<StarXpandCommandBuilder>
+    {
+        [ReactMethod("init")]
+        public void Init(IReactPromise<string> promise)
+        {
+            StarXpandCommandBuilder nativeObject = new StarXpandCommandBuilder();
+
+            SetObject(nativeObject, out string objectIdentifier);
+
+            promise.Resolve(objectIdentifier);
+        }
+
+        [ReactMethod("dispose")]
+        public void Dispose(string objectIdentifier, IReactPromise<JSValue.Void> promise)
+        {
+            DisposeObject(objectIdentifier);
+
+            promise.Resolve();
+        }
+
+        [ReactMethod("setPreSetting")]
+        public void SetPreSetting(string objectIdentifier, string preSettingBuilderIdentifier, IReactPromise<JSValue.Void> promise)
+        {
+         
